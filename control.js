d07bfba7ff5660d72be307cd20d25c5c4d0257af
winnieSikinyi <winniesikinyi@gmail.com>
2023-04-07 14:42:41 +0300

    changes

--- control.js
+++ control.js
@@ -14,45 +14,37 @@ arr(str)
 //Write a function that accepts an array of numbers and uses the forEach()
 // method to console.log each number multiplied by 2.
 
+function items (Arry){
 
-//multiplication
-// let nums = [25,23,34,53,56,7];
-// let j = 0;
-// let product = 2;
-// while(j< nums.length){
-//     product *= nums [j] 
-//     j++;
-// }
-// console.log({product});
+    let result = [];
+    Arry.forEach(function(item){
+        result.push(item*2)
+        
+    });
+    console.log(result);
+}
+let Arry = [12,2,4,3,5,7];
+items(Arry)
 
-// //
-// function num (numsy){
-//     num.forEach(num)
-
-// }
-
-// let nim = [25,23,34,53,56,7];
-// nim.forEach(myFunction)
-// let t = 0
-// let multiplication = 2
-// function myFunction(t,index,multiplication){
-// t[index] =multiplication*2
-// }
 
 
 
 //Write a function that takes in an array of numbers and consoles the first four 
 // items multiplied by8 and the last two added by 5. Console the array with the new values
-let arrayNum = [25,23,34,53,56,7];
+
 function arry(arrayNum){
-    let j = 0;
-    let product = 8;
-     while(j< nums.length){
-        product *= nums [j] 
-         j++;
+    for (let i=0;i<4;i++){
+        arrayNum[i] *=8;
     }
-    console.log({product});
+
+    for (let i = arrayNum.length -2; i < arrayNum.length; i++){
+        arrayNum[i] += 5
+    }
+ console.log(arrayNum);
 }
+let arrayNum = [25,23,34,53,56,7];
+arry(arrayNum)
+
 
 
 
